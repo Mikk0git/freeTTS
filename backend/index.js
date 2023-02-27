84e364c4e754544773ffc77e08743044b1797931
Mikk0git <kasprzakmilan1@gmail.com>
2023-02-27 20:39:14 +0100

    python child process

--- backend/index.js
+++ backend/index.js
@@ -11,19 +11,41 @@ app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
+app.listen(
+    PORT,
+    () => console.log('http://localhost:'+ PORT)
+)
+
 
 app.post('/prompt', (req, res) => {
   const language = req.body.language;
   const text = req.body.text;
 
-  // Wykonaj odpowiednie operacje na podstawie przesłanych danych
+
     console.log("text = "+text)
     console.log("lang = "+ language)
+
+    const { spawn } = require('child_process');
+
+    // Uruchomienie skryptu Pythona jako proces podrzędny
+    const pythonProcess = spawn('python', ['app.py', text, language]);
+
+    // Przechwytywanie danych z wyjścia standardowego procesu Pythona
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`Received data from Python: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Error received from Python: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+      console.log(`Python process exited with code ${code}`);
+    });
+
 
   res.status(200).json({ message: 'Success' }); // Zwróć odpowiedź w formacie JSON
 });
 
-app.listen(
-    PORT,
-    () => console.log('http://localhost:'+ PORT)
-)
+
+
